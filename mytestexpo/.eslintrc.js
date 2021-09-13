72d7b03e408292b1952855ba02f62461ebe32dc3
remagart <jfmamjjasond418@gmail.com>
2021-09-13 16:45:54 +0800

    prettier

--- mytestexpo/.eslintrc.js
+++ mytestexpo/.eslintrc.js
@@ -19,7 +19,8 @@ module.exports = {
     },
     "plugins": [
         "react",
-        "@typescript-eslint"
+        "@typescript-eslint",
+        "prettier"
     ],
     "rules": {
         "indent": [
