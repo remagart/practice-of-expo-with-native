958cd167f9359363ac1bcc55da3c729b9b0be4b5
remagart <jfmamjjasond418@gmail.com>
2021-09-17 09:26:42 +0800

    測試圖片用法

--- mytestexpo/src/screens/TabTwoScreen.tsx
+++ mytestexpo/src/screens/TabTwoScreen.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, Image } from "react-native";
 
 import EditScreenInfo from "../../components/EditScreenInfo";
 import { Text, View } from "../../components/Themed";
@@ -10,7 +10,8 @@ export default function TabTwoScreen() {
     <View style={styles.container}>
       <Text style={styles.title}>Tab Two</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+      <Image source={require("../../assets/myimg/dog.jpeg")} style={{ width: 200, height: 200 }} />
+      {/* <EditScreenInfo path="/screens/TabTwoScreen.tsx" /> */}
     </View>
   );
 }
